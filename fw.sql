7d8b1044f48589bb631646e4524c8c9f35d96475
Michael <49639084+php-8@users.noreply.github.com>
2019-05-03 19:01:39 +0300

    Add files via upload
    sql added

---
+++ fw.sql
@@ -0,0 +1,175 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.4
+-- https://www.phpmyadmin.net/
+--
+-- Хост: 127.0.0.1
+-- Время создания: Май 03 2019 г., 18:00
+-- Версия сервера: 10.1.37-MariaDB
+-- Версия PHP: 7.3.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- База данных: `fw`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `categories`
+--
+
+CREATE TABLE `categories` (
+  `id` int(11) NOT NULL,
+  `title` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `parent` int(11) NOT NULL,
+  `alias` varchar(255) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Дамп данных таблицы `categories`
+--
+
+INSERT INTO `categories` (`id`, `title`, `parent`, `alias`) VALUES
+(685, 'Комплектующие к Apple', 0, 'komplektuyuschie-k-apple'),
+(691, 'Запчасти iPad', 685, 'zapchasti-ipad'),
+(692, 'Запчасти iPhone', 685, 'zapchasti-iphone'),
+(693, 'Запчасти iPod', 685, 'zapchasti-ipod'),
+(694, 'Запчасти Mac', 685, 'zapchasti-mac'),
+(695, 'iPad', 691, 'ipad'),
+(696, 'iPad 2', 691, 'ipad-2'),
+(697, 'iPad NEW (iPad 3)', 691, 'ipad-new-ipad-3'),
+(698, 'iPad 4', 691, 'ipad-4'),
+(699, 'iPad mini', 691, 'ipad-mini'),
+(700, 'iPhone', 692, 'iphone'),
+(701, 'iPhone 3G/3GS', 692, 'iphone-3g-3gs'),
+(702, 'iPhone 4', 692, 'iphone-4'),
+(703, 'iPhone 4S', 692, 'iphone-4s'),
+(704, 'iPhone 5', 692, 'iphone-5'),
+(705, 'Микросхемы Apple', 685, 'mikroshemy-apple'),
+(836, 'Защитные плёнки на Apple', 0, 'zaschitnye-plenki-na-apple'),
+(840, 'iPad', 836, 'ipad-840'),
+(841, 'iPhone', 836, 'iphone-841'),
+(842, 'iPod', 836, 'ipod'),
+(843, 'Mac', 836, 'mac'),
+(853, 'Оборудование для ремонта Apple', 0, 'oborudovanie-dlya-remonta-apple'),
+(876, 'Аксессуары для Apple', 0, 'aksessuary-dlya-apple'),
+(877, 'Аксессуары iPad', 876, 'aksessuary-ipad'),
+(878, 'Аксессуары iPhone', 876, 'aksessuary-iphone'),
+(879, 'Аксессуары iPod', 876, 'aksessuary-ipod'),
+(880, 'Аксессуары Mac', 876, 'aksessuary-mac'),
+(881, 'iPad', 877, 'ipad-881'),
+(882, 'iPad 2', 877, 'ipad-2-882'),
+(883, 'iPad NEW 3 / iPad 4', 877, 'ipad-new-3-ipad-4'),
+(884, 'iPad mini', 877, 'ipad-mini-884'),
+(885, 'iPhone 3G / 3GS', 878, 'iphone-3g-3gs-885'),
+(886, 'iPhone 4 / 4S', 878, 'iphone-4-4s'),
+(887, 'iPhone 5', 878, 'iphone-5-887'),
+(888, 'Аксессуары для Apple', 876, 'aksessuary-dlya-apple-888'),
+(895, 'iPhone 5 Lamborghini', 878, 'iphone-5-lamborghini');
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `post`
+--
+
+CREATE TABLE `post` (
+  `id` int(11) NOT NULL,
+  `category_id` int(11) NOT NULL,
+  `title` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `excerpt` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `text` text COLLATE utf8_unicode_ci NOT NULL,
+  `keywords` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `description` varchar(255) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Дамп данных таблицы `post`
+--
+
+INSERT INTO `post` (`id`, `category_id`, `title`, `excerpt`, `text`, `keywords`, `description`) VALUES
+(1, 2, 'Создание фреймврка', 'Фреймворк с нуля', 'There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don\'t look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn\'t anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.', 'Свой фреймворк', 'Создание фреймврка'),
+(2, 2, 'Создание фреймврка', 'Фреймворк с нуля', 'There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don\'t look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn\'t anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.', 'Свой фреймворк', 'Создание фреймврка');
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `user`
+--
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL,
+  `userlogin` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `password` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `email` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `role` enum('user','admin') COLLATE utf8_unicode_ci NOT NULL DEFAULT 'user'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Дамп данных таблицы `user`
+--
+
+INSERT INTO `user` (`id`, `userlogin`, `password`, `email`, `name`, `role`) VALUES
+(11, 'user54656@user.com', 'admin456456', 'michaelphp@yandex.ru456', '', 'user');
+
+--
+-- Индексы сохранённых таблиц
+--
+
+--
+-- Индексы таблицы `categories`
+--
+ALTER TABLE `categories`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Индексы таблицы `post`
+--
+ALTER TABLE `post`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Индексы таблицы `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `login` (`userlogin`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
+-- AUTO_INCREMENT для сохранённых таблиц
+--
+
+--
+-- AUTO_INCREMENT для таблицы `categories`
+--
+ALTER TABLE `categories`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=896;
+
+--
+-- AUTO_INCREMENT для таблицы `post`
+--
+ALTER TABLE `post`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT для таблицы `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
